2cf09900855d85ea1b9e20c20176807b9c277b26
unknown <dikeyparen@yandex.ru>
2022-10-09 23:08:33 +0400

    added folders

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+import cv2
+import requests
+import os
+
+URL_zebra = "https://yandex.ru/images/search?text=zebra"
+URL_bay_horse = "https://yandex.ru/images/search?text=bay%20horse"
+
+if not os.path.isdir("dataset"):
+    os.mkdir("dataset")
+    if not os.path.isdir("zebra"):
+        os.makedirs("dataset/zebra")
+    if not os.path.isdir("bay_horse"):
+        os.makedirs("dataset/bay_horse")
+
+
+
+
+
+
+
+
